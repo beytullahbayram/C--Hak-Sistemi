62c9c804809e085e020a1e6aafd65f80e99b9000
beytufener12 <61604771+beytufener12@users.noreply.github.com>
2021-08-21 12:51:53 +0300

    C# ile Giriş ekranı hak sistemi
    c# kullanarak giriş ekranlarına hak sistemi koyma.

---
+++ HakSistemi/HakSistemi/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HakSistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        int hak = 3;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if(textBox1.Text == "" &&  textBox2.Text == "")
+            {
+                hak--;
+                MessageBox.Show("Kullanıcı Adı veya Şifre Boş ! Kalan Hakkınız :" + hak);
+            }
+
+            else if (textBox1.Text == "kullaniciadi" && textBox2.Text == "sifre")
+            {
+                MessageBox.Show("Giriş Başarılı");
+                
+                
+            }
+
+            else
+            {
+                hak--;
+                MessageBox.Show("Kullanıcı Adı veya Şifre Hatalı ! Kalan Hakkınız :" + hak);
+            }
+
+            if( hak == 0)
+            {
+                MessageBox.Show("Hakkınız Bitmiştir.Program Kapatılıyor");
+                Application.Exit();
+            }
+
+
+            
+        }
+    }
+}
